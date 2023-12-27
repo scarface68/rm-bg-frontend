a0fb08eeb340ccab284808313307af1d9cad62b2
Sai Chandan Mohanty <saichandan68@gmail.com>
2023-12-28 00:20:58 +0530

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./components/Header";
 import BackgroundRemover from "./components/BackgroundRemover";
+import Footer from "./components/Footer";
 function App() {
   return (
     <div className="App">
       <Header />
       <BackgroundRemover />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="footer" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', color: 'white', position: 'fixed', bottom: 0, left: 0, right: 0 }}>
+            <p>Made with ❤️ <a href="https://github.com/scarface68" style={{ color: 'white' }}>scarface68</a></p>
+        </footer>
+    );
+};
+
+export default Footer;
